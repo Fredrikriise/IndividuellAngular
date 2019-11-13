41156cc323ab753a978bca27d739b1f539f89587
Ocianus <fredrik1998@hotmail.com>
2019-11-13 23:21:14 +0100

    La til ToDo fil

---
+++ IndividuellAngular/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndividuellAngular
+{
+    public class ToDo
+    {
+
+        //Bruke to constructors i samme typescript? Evt NgOnInit?
+        //To typescripts som har samme selector?
+    }
+}
